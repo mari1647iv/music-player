a0dc7ffd2e8e445fca6924482163d61675a3d454
mari1647iv <ima1647@outlook.com>
2021-09-15 00:26:27 +0300

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./routes/Main/Main";
--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Main.css";
-import Button from "../../components/Button/Button";
-import {Play} from "react-feather";
 
 function Main() {
     return ( 
