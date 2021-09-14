d5cb3a948d0cc44083d930d242d9fd1f6569884c
mari1647iv <ima1647@outlook.com>
2021-09-14 22:12:37 +0300

    Add Button component

--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Main.css";
-
+import Button from "../../components/Button/Button";
 function Main() {
     return ( 
-        <div className="main"> Hello, world! </div>
+        <div className="main-page"> 
+            <p>Hello, world!</p>
+            <div>
+                <Button type="colorful" children="Search"/> 
+                <Button type="icon" children="Search"/> 
+                <Button type="stuff" children="Search"/> 
+            </div><div>
+                <Button type="colorful" children="Search" isDisabled={true}/> 
+                <Button type="icon" children="Search" isDisabled={true}/> 
+                <Button type="stuff" children="Search" isDisabled={true}/> 
+            </div>
+        </div>
     );
 }
 
