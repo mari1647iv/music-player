1f9b826f6f2a1be1343b76b6abb8def4136a56da
mari1647iv <ima1647@outlook.com>
2021-09-14 22:24:54 +0300

    Install feather-icons

--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Main.css";
 import Button from "../../components/Button/Button";
+import {Play} from "react-feather";
+
 function Main() {
     return ( 
         <div className="main-page"> 
@@ -8,10 +10,12 @@ function Main() {
             <div>
                 <Button type="colorful" children="Search"/> 
                 <Button type="icon" children="Search"/> 
+                <Button type="icon" children={<Play size={40}/>}/> 
                 <Button type="stuff" children="Search"/> 
             </div><div>
                 <Button type="colorful" children="Search" isDisabled={true}/> 
                 <Button type="icon" children="Search" isDisabled={true}/> 
+                <Button type="icon" children={<Play size={40}/>} isDisabled={true}/> 
                 <Button type="stuff" children="Search" isDisabled={true}/> 
             </div>
         </div>
