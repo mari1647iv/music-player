787c9d9ca2a72e6d3b871d6d608430745c3cbc93
mari1647iv <ima1647@outlook.com>
2021-09-17 17:37:57 +0300

    Fix 'nested ternary operation' issue from SonarCloud

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -7,8 +7,8 @@ function Input(props) {
 
     return (
         <div className="input-container">
-            <i className="input-icon">{(type==="music"||type==="events")?<Search size={36}/>:type==="date"?<Calendar size={36}/>:""}</i>
-            <input type={type==="date"?"date":"text"} className="input-box" placeholder={type==="music"?"Search Music":type==="events"?"Search Events":"Type Some Text"}/>
+            <i className="input-icon">{(type==="music"||type==="events")?<Search size={36}/>:(type==="date"?<Calendar size={36}/>:"")}</i>
+            <input type={type==="date"?"date":"text"} className="input-box" placeholder={type==="music"?"Search Music":(type==="events"?"Search Events":"Type Some Text")}/>
         </div>
 
     );
