cb3cf53214cebccd7c4ca7e32e7934e2ad785973
mari1647iv <ima1647@outlook.com>
2021-09-17 17:58:35 +0300

    Fix 'nested ternary operation' issue from SonarCloud

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,11 +4,17 @@ import {Search, Calendar} from "react-feather";
 
 function Input(props) {
     const {type} = props;
+    let icon;
+    let placeholder="Type Some Text";
+
+    if (type==="music") { icon = <Search size={36}/>; placeholder = "Search Music"; }
+    else if(type==="events") { icon= <Search size={36}/>; placeholder = "Search Events"; }
+         else if (type==="date") icon = <Calendar size={36}/>;
 
     return (
         <div className="input-container">
-            <i className="input-icon">{(type==="music"||type==="events")?<Search size={36}/>:(type==="date"?<Calendar size={36}/>:"")}</i>
-            <input type={type==="date"?"date":"text"} className="input-box" placeholder={type==="music"?"Search Music":(type==="events"?"Search Events":"Type Some Text")}/>
+            <i className="input-icon">{icon}</i>
+            <input type={type==="date"?"date":"text"} className="input-box" placeholder={placeholder}/>
         </div>
 
     );
