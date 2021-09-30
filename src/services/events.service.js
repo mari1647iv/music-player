0eee57cbd677601f0f1ecd61db180f74e6e55f00
mari1647iv <ima1647@outlook.com>
2021-09-30 21:13:50 +0300

    Add filter events functionality

--- src/services/events.service.js
+++ src/services/events.service.js
@@ -6,9 +6,19 @@ class EventsService {
   API_KEY = 'ayo0ADNZo1OQi066'
 
   async getEvents(artist, from, to) {
+    if (!to.length) {
+      to = '3000-12-31'
+    }
+    if (!from.length) {
+      let today = new Date()
+      from =
+        today.getFullYear() + '-' + (today.getMonth() < 9 ? '0' : '') + (today.getMonth() + 1) + '-' + today.getDate()
+    }
+
     const response = await axios.get(this.API_ENDPOINT, {
       params: { apikey: this.API_KEY, artist_name: artist, min_date: from, max_date: to }
     })
+
     const data = response.data
 
     return data
